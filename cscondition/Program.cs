42d548a86dc110710a3dd4af24c121f10d68f3ba
2uijin <49094082+2uijin@users.noreply.github.com>
2020-05-06 08:28:13 +0900

    초기 커밋.

---
+++ cscondition/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cscondition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("숫자입력 : ");
+            var input = int.Parse(Console.ReadLine());
+
+            if (input % 2 == 0) {
+                Console.WriteLine("짝수 입니다!");
+            }
+
+            if (input % 2 == 0)
+            {
+                Console.WriteLine("홀수 입니다!");
+            }
+
+
+        }
+    }
+}
